350b1549d7b30da7733a7303604cb349038c8f0f
omama1016 <along1016@163.com>
2022-07-29 10:47:48 +0800

    lab2 finished

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,13 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) return B;
+        IntList ptr = A;
+        while (ptr.rest != null){
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +97,18 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) return B;
+
+        IntList p = new IntList(A.first, null);
+        IntList cur = p;
+        A = A.rest;
+        while (A != null){
+            cur.rest = new IntList(A.first, null);
+            A = A.rest;
+            cur = cur.rest;
+        }
+        cur.rest = B;
+        return p;
     }
 
 
