0532edcf449071d5492373ff53782af9ba7346b1
Jason <jason@JasondeMacBook-Pro.local>
2022-08-01 16:10:10 +0800

    lab3 finished

---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,8 @@
+package com.java.lab3.Flik;
+
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    public static boolean isSameNumber(Integer a, Integer b) {
+        return a == b;
+    }
+}
