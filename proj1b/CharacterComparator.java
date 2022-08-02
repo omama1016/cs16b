69ab59f4e2464f525841a864b283b9f95bd13846
omama1016 <31500904+omama1016@users.noreply.github.com>
2022-08-02 11:26:15 +0800

    Update CharacterComparator.java

--- proj1b/CharacterComparator.java
+++ proj1b/CharacterComparator.java
@@ -1,5 +1,3 @@
-package com.wzu.wg.proj1B;
-
 /** This interface defines a method for determining equality of characters. */
 public interface CharacterComparator {
     /** Returns true if characters are equal by the rules of the implementing class. */
