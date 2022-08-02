945bf0ab5b4063ff04f658fa6dd17b32597e88f0
Jason <jason@JasondeMacBook-Pro.local>
2022-08-02 17:22:31 +0800

    project 1b completed...

---
+++ proj1b/Deque.java
@@ -0,0 +1,13 @@
+package com.java.proj01b;
+
+public interface Deque<T> {
+    public void addFirst(T item);
+    public void addLast(T item);
+    public boolean isEmpty();
+    public int size();
+    public void printDeque();
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+}
+
