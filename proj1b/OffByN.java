d8be8d1ca6bad6cc683b69f70696f6fc2d5a66d7
omama1016 <along1016@163.com>
2022-08-02 11:23:46 +0800

    project1b completed...

---
+++ proj1b/OffByN.java
@@ -0,0 +1,22 @@
+package com.wzu.wg.proj1B;
+
+public class OffByN implements CharacterComparator {
+
+    private int N;
+
+//    public OffByN(){
+//
+//    }
+
+    public OffByN(int N) {
+        this.N = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        if (Math.abs(x - y) == N) {
+            return true;
+        }
+        return false;
+    }
+}
